eb901fc8305443b7420cb97e4d1e326e0a48cb55
hyeonjun4405 <139756506+hyeonjun4405@users.noreply.github.com>
2026-05-02 00:08:05 +0900

    Create movies.js

---
+++ netlify/functions/movies.js
@@ -0,0 +1,51 @@
+const admin = require("firebase-admin");
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+  });
+}
+
+const db = admin.firestore();
+
+exports.handler = async function () {
+  try {
+    const snapshot = await db.collection("movies").get();
+
+    const movies = snapshot.docs.map((doc) => {
+      const data = doc.data();
+
+      return {
+        id: doc.id,
+        title: data.title ?? "",
+        rating: data.rating ?? null,
+        oneLine: data.review ?? "",
+        review: data.detailedReview ?? "",
+       
+      };
+    });
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(movies, null, 2),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify({
+        error: error.message,
+      }),
+    };
+  }
+};
